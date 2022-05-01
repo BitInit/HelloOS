df2ef2b285c4030ec0f3f732ba2270ae42013599
John Wang <john.wang.bit@gmail.com>
2022-05-01 23:12:45 +0800

    printk function

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,57 @@
+#include "lib.h"
+#include "printk.h"
 
 void Start_Kernel(void) {
-    while (1) 
+    int *addr = (int *)0xffff800000a00000;
+	int i;
+
+	Pos.XResolution = 1440;
+	Pos.YResolution = 900;
+
+	Pos.XPosition = 0;
+	Pos.YPosition = 0;
+
+	Pos.XCharSize = 8;
+	Pos.YCharSize = 16;
+
+	Pos.FB_addr = (int *)0xffff800000a00000;
+	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
+
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+
+	color_printk(YELLOW,BLACK,"Hello\t\t World!\n");
+
+    while (1)
         ;
 }
